9914def14cab9f917677f6ed39340f0fa1d4b9b4
NelsonGomesNeto <ngn@ic.ufal.br>
2018-08-16 17:02:18 -0300

    LET'S MAKE THIS HAPPEN

---
+++ parser.py
@@ -0,0 +1,10 @@
+def readER():
+    er = {}
+    while (True):
+        line = input()
+        if (line == "END"): break
+        line.split
+    return(er)
+
+er = readER()
+print("Reading:", input())
